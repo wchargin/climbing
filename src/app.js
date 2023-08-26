9794fa4ab73ade05d54ac91157ab90ab6ed82e41
Willow Chargin <wchargin@gmail.com>
2023-08-25 20:58:28 -0700

    gallery: make header more visible

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ function App() {
   const dataDesc = data.slice().reverse();
   return (
     <main>
-      <h1 className="text-xl font-bold pt-4">Routes climbed</h1>
+      <h1 className="text-4xl font-bold pt-8 pb-4 mx-2">Routes climbed</h1>
       <div className="routes-grid grid gap-4 p-2">
         {dataDesc.map((route) => (
           <Route key={route.id} route={route} />
