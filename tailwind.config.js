63ac128ad4e6c210354dc330eb2c40a707934867
Willow Chargin <wchargin@gmail.com>
2023-08-25 19:46:59 -0700

    css: use desaturated slate palette for brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,28 @@
+const colorConvert = require("color-convert");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.js"],
   theme: {
     extend: {
       colors: {
-        brand: {
-          700: "#384144",
-          800: "#253338",
-        },
+        brand: brandPalette(),
       },
     },
   },
   plugins: [],
 };
+
+function brandPalette() {
+  const palette = {};
+  for (const [k, v] of Object.entries(colors.slate)) {
+    palette[k] = desaturateHex(v, 1.5);
+  }
+  return palette;
+}
+
+function desaturateHex(hex, exponent) {
+  let [h, s, l] = colorConvert.hex.hsl(hex);
+  s = 100 * Math.pow(s / 100, exponent);
+  return "#" + colorConvert.hsl.hex(h, s, l).toLowerCase();
+}
