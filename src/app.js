2736a21ee59cd92bbd9cb5b85bbc1e5bd5733f9c
Willow Chargin <wchargin@gmail.com>
2023-08-25 17:59:49 -0700

    index: show routes in a gallery view

--- src/app.js
+++ src/app.js
@@ -1,5 +1,38 @@
-function Greet() {
-  return <h1>Hello, world!</h1>;
+import data from "./data.json";
+
+function App() {
+  const dataDesc = data.slice().reverse();
+  return (
+    <main>
+      <h1>Routes climbed</h1>
+      <div className="routes">
+        {dataDesc.map((route) => (
+          <Route key={route.id} route={route} />
+        ))}
+      </div>
+    </main>
+  );
 }
 
-export default Greet;
+function Route({ route }) {
+  const title = `${route.category.replace(/\b./g, (c) => c.toUpperCase())} #${
+    route.indexInCategory
+  }`;
+  return (
+    <figure className="route">
+      <a href={imageUrl(route.id, "full")}>
+        <img src={imageUrl(route.id, "400")} alt={`Photo of ${title}`} />
+      </a>
+      <figcaption>
+        #{route.id} ({title})
+      </figcaption>
+    </figure>
+  );
+}
+
+function imageUrl(id, size) {
+  const imageId = String(id).padStart(4, "0");
+  return `https://storage.googleapis.com/wchargin-climbing-public/${size}/${imageId}.jpg`;
+}
+
+export default App;
--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ function main() {
 <html lang="en">
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="styles.css">
 </head>
 <body>
