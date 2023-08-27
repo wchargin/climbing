85b666ab5ff1913ab796d7b96416d92a6f0880e8
Willow Chargin <wchargin@gmail.com>
2023-08-27 13:28:58 -0700

    server: render pages for each climbing route
    One at the global index (e.g., `/routes/127/`) and one at the index
    within category (e.g., `/routes/blue/20/`).

--- src/server.js
+++ src/server.js
@@ -1,33 +1,59 @@
 import fs from "fs";
+import pathLib from "path";
 
 import * as Server from "react-dom/server";
 
 import App from "./app";
+import data from "./data";
 
 async function main() {
   const args = process.argv.slice(2);
-  if (args.length !== 2) {
-    throw new Error(`usage: node server.js <path> <outfile>`);
+  if (args.length !== 1) {
+    throw new Error(`usage: node server.js <outdir>`);
   }
-  const [path, outfile] = args;
+  const [outdir] = args;
 
+  const paths = ["/"];
+  for (const route of data) {
+    paths.push(`/routes/${route.id}/`);
+    paths.push(`/routes/${route.category}/${route.indexInCategory}/`);
+  }
+
+  await Promise.all(
+    paths.map((path) => {
+      const fsPath = pathLib.join(outdir, ...path.split("/"));
+      return renderPage(path, fsPath, "index.html");
+    }),
+  );
+}
+
+async function renderPage(path, outdir, filename) {
+  console.warn("rendering %s -> %s/%s", path, outdir, filename);
   const rendered = Server.renderToString(<App path={path} />);
+  const toRoot = pathToRoot(path);
   const page = `\
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width">
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="${toRoot}styles.css">
 </head>
 <body data-path="${escapeAttr(path)}">
 <div id="root">${rendered}</div>
-<script src="client.js"></script>
+<script src="${toRoot}client.js"></script>
 </body>
 </html>
 `;
 
-  await fs.promises.writeFile(outfile, page);
+  await fs.promises.mkdir(outdir, { recursive: true });
+  await fs.promises.writeFile(pathLib.join(outdir, filename), page);
+}
+
+function pathToRoot(path) {
+  if (!path.startsWith("/")) throw new Error("Missing leading slash: " + path);
+  path = path.slice(1);
+  return path.replace(/[^/]*\//g, "../").replace(/[^/]*$/, "");
 }
 
 function escapeAttr(text) {
