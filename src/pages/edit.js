02eb97407194276f47ea941c6714017822e85a1d
Willow Chargin <wchargin@gmail.com>
2023-09-07 21:52:27 -0700

    edit: add rudimentary editor for route data

--- src/pages/edit.js
+++ src/pages/edit.js
@@ -37,20 +37,127 @@ function useNotesState() {
   return { raw, parsed, setRaw, setParsed };
 }
 
-function Edit() {
-  const notes = useNotesState();
+function Cell({ label, children }) {
+  return (
+    <label className="flex flex-col">
+      <span className="text-xs text-brand-300">{label}</span>
+      {children}
+    </label>
+  );
+}
 
-  const textareaClasses = "bg-brand-900 w-full min-h-[40rem]";
+function isPositiveIntStr(str) {
+  const num = Number(str);
+  return String(num) === str && Number.isInteger(num) && num > 0;
+}
+const DATE_RE = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
+
+function Edit() {
+  const [headers, setHeaders] = useState({
+    id: "",
+    category: "black",
+    indexInCategory: "",
+    date: "",
+    location: "poplar",
+    thumbhash: "",
+  });
+  const notes = useNotesState();
+  const output = JSON.stringify(
+    {
+      ...headers,
+      id: Number(headers.id),
+      indexInCategory: Number(headers.indexInCategory),
+      notes: notes.parsed.ok ? JSON.parse(notes.parsed.value) : [],
+    },
+    null,
+    2,
+  );
+
+  const setHeader = (key) => (e) =>
+    setHeaders((h) => ({ ...h, [key]: e.target.value }));
+
+  const clsInput = "bg-brand-900 text-sm py-px px-1 h-6";
+  const clsSelect = classNames(clsInput, "focus-within:ring-2");
+  const clsNotesTextarea = classNames(clsInput, "w-full min-h-[36rem]");
+  const clsInvalid = "!bg-red-900";
 
   return (
     <main className="flex flex-col gap-3 m-4">
-      <h1 className="text-2xl">Notes</h1>
+      <h1 className="text-2xl">Edit route</h1>
+      <div className="flex gap-3">
+        <Cell label="id">
+          <input
+            type="text"
+            value={headers.id}
+            onChange={setHeader("id")}
+            className={classNames(
+              clsInput,
+              isPositiveIntStr(headers.id) || clsInvalid,
+            )}
+            size={3}
+          />
+        </Cell>
+        <Cell label="category">
+          <select
+            value={headers.category}
+            onChange={setHeader("category")}
+            className={clsSelect}
+          >
+            <option>black</option>
+            <option>blue</option>
+            <option>pink</option>
+          </select>
+        </Cell>
+        <Cell label="index">
+          <input
+            type="text"
+            value={headers.indexInCategory}
+            onChange={setHeader("indexInCategory")}
+            className={classNames(
+              clsInput,
+              isPositiveIntStr(headers.indexInCategory) || clsInvalid,
+            )}
+            size={3}
+          />
+        </Cell>
+        <Cell label="date">
+          <input
+            type="text"
+            value={headers.date}
+            onChange={setHeader("date")}
+            className={classNames(
+              clsInput,
+              headers.date.match(DATE_RE) || clsInvalid,
+            )}
+            size={8}
+          />
+        </Cell>
+        <Cell label="location">
+          <select
+            value={headers.location}
+            onChange={setHeader("location")}
+            className={clsSelect}
+          >
+            <option>poplar</option>
+            <option>fremont</option>
+          </select>
+        </Cell>
+        <Cell label="thumbhash">
+          <input
+            type="text"
+            value={headers.thumbhash}
+            onChange={setHeader("thumbhash")}
+            className={classNames(clsInput, "font-mono text-sm")}
+            size={12}
+          />
+        </Cell>
+      </div>
       <div className="flex gap-3">
         <textarea
           value={notes.raw.value}
           onChange={(e) => notes.setRaw(e.target.value)}
           className={classNames(
-            textareaClasses,
+            clsNotesTextarea,
             notes.raw.ok || "text-red-500",
           )}
         />
@@ -58,11 +165,16 @@ function Edit() {
           value={notes.parsed.value}
           onChange={(e) => notes.setParsed(e.target.value)}
           className={classNames(
-            textareaClasses,
+            clsNotesTextarea,
             notes.parsed.ok || "text-red-500",
           )}
         />
       </div>
+      <textarea
+        readOnly
+        value={output}
+        className={classNames(clsInput, "h-[12rem]")}
+      />
     </main>
   );
 }
