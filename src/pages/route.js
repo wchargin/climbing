81b5808f3dcb65f117ce5d89fa9e9f944102f6c3
Willow Chargin <wchargin@gmail.com>
2023-11-12 14:46:39 -0800

    route: remove dead style from h1

--- src/pages/route.js
+++ src/pages/route.js
@@ -77,7 +77,7 @@ function Route({ id }) {
       </div>
       <div className="mt-12 md:mt-0 tall:mt-12 w-full h-full lg:max-w-[600px] px-6 lg:px-0">
         <div className="flex items-center">
-          <h1 className="text-4xl outline-l-4 flex-grow">
+          <h1 className="text-4xl flex-grow">
             {capitalize(route.category)} #{route.indexInCategory}
           </h1>
           <NavAdjacentLink
